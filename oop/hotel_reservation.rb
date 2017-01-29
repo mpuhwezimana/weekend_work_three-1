256b2ac0bd0bdef1af4c933f4a8dc7e5774754fb
mpuhwezimana <mpuhwearsene.com>
2017-01-30 01:04:22 +0200

    Worked on Testing hotel reservation code.

--- oop/hotel_reservation.rb
+++ oop/hotel_reservation.rb
@@ -32,3 +32,59 @@ end
 # The add_a_crib method
 # The add_a_custom_amenity method
 
+puts "TESTING HotelReservation ....."
+puts
+
+reservation = HotelReservation.new({customer_name: "Arsene Mpuhwe", data: "28 Apr 2017", room_number: 15})
+
+reservation.room_number = 15
+result = reservation.room_number
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 15
+  puts "PASS"
+else
+  puts "FAIL"
+end
+
+reservation.add_a_fridge
+result = reservation.amenities[0]
+
+puts "Your method returned:"
+puts result
+puts
+                             
+if result == "fridge"
+  puts "PASS"
+else
+  puts "FAIL"
+end
+
+reservation.add_a_crib
+result = reservation.amenities[1]
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == "crib"
+  puts "PASS"
+else
+  puts "FAIL"
+end
+
+reservation.add_a_custom_amenity("Cofee")
+result = reservation.amenities[2]
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == "Cofee"
+  puts "PASS"
+else
+  puts "FAIL"
+end
