f469b29add1cff65da83facb7424fd35f648c3ab
mpuhwezimana <mpuhwearsene.com>
2017-01-30 01:27:57 +0200

    worked on Tv and Remote OOP.

--- oop/tv_and_remote.rb
+++ oop/tv_and_remote.rb
@@ -10,3 +10,97 @@
 # increment_volume (this will increase the tv's volume by 1)
 # decrement_volume (this will decrease the tv's volume by 1)
 # set_channel (this will change the tv's channel to whatever integer is passed to this method)
+
+class Remote
+  attr_accessor :tv
+  def initialize(remote_attribute)
+    @televisions =remote_attribute[:tv]
+  end
+
+  def toggle_power
+    if @power == "on"
+      puts "off"
+    else
+      puts "on"
+    end
+  end
+
+  def increment_volume
+    @volume += 1
+  end
+
+  def decrement_volume
+    @volume -= 1
+  end
+
+  def set_channel(integer)
+    @channel == integer
+  end
+end
+
+class Tv < Remote
+  attr_accessor :power, :volume , :channel
+  def initialize(tv_attributes)
+    super
+    @power = tv_attributes[:power]
+    @volume = tv_attributes[:volume]
+    @channel = "unkown"
+  end
+end
+
+
+# The driver code
+
+puts "TESTING in progress..."
+puts
+
+tv1 = Tv.new({power: "off", volume: 10, channel: "4"})
+remote = Remote.new({tv:[tv1]})
+
+result = tv1.toggle_power
+puts "Power status returned:"
+puts
+
+if result == "on"
+  puts "PASS!"
+else
+  puts "F"
+end
+
+puts
+result = tv1.increment_volume
+puts "Volume returned:"
+puts result
+
+puts
+
+if result == 11
+  puts "PASS!"
+else
+  puts "F"
+end
+puts
+
+result = tv1.decrement_volume
+
+puts "volume returned:"
+puts result
+puts
+
+if result == 10
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = tv1.set_channel(10)
+
+puts "Channel returned:"
+puts result
+puts
+
+if result == 10
+  puts "PASS!"
+else
+  puts "F"
+end
